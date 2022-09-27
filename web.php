4142e3ce10c11711c7f69263123ed9c82826cc60
Wahyu Styo Pratama <42894387+wahyupratama18@users.noreply.github.com>
2022-09-27 09:42:42 +0700

    Revert "Add files via upload"

--- web.php
+++ web.php
@@ -1,18 +1,7 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('file-upload', function () {
-    return view('default');
-});
+Route::get('/post.create', [PostController::class, 'create']);
+Route::get('/post.index', [PostController::class, 'store']);
+Route::get('/post.store', [PostController::class, '']);
